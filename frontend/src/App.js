a5170ebd59fcf3c63e159559fe1ab1af204f0884
Sonbom Lee <sonbomlee97@gmail.com>
2025-03-09 18:55:55 -0700

    Discarded 'prettier'
    Discarded 'prettier'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,17 +19,7 @@ function App() {
         <h2>Backend says: {message}</h2>
         
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
