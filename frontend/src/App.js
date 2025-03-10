ba310537d50ed1f2d2636dfd88b451255ad31794
Sonbom Lee <sonbomlee97@gmail.com>
2025-03-09 18:44:03 -0700

    Divided Frontend/Backend
    1. Frontend/Backend divided
    2. Frontend listens to backend
    3. Backend environment setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000/')
+        .then(response => response.text())
+        .then(data => setMessage(data));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
+        
+        <h1>Frontend Running!</h1>
+        <h2>Backend says: {message}</h2>
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
